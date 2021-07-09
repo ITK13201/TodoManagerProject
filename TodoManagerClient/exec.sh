559d20c5544a7ba5da7abe6d9d0c9875015b45e4
ITK13201 <tkik13201@gmail.com>
2021-07-09 23:29:28 +0900

    fix: client bug

--- TodoManagerClient/exec.sh
+++ TodoManagerClient/exec.sh
@@ -5,6 +5,8 @@ cd `dirname $0`
 BUILD_PATH="./out/production/TodoManagerClient"
 LIB_PATH="./lib/gson-2.8.7.jar:./lib/lombok-1.18.20.jar:./lib/commons-lang3-3.12.0.jar"
 
+mkdir -p $BUILD_PATH
+
 export CLASSPATH="$BUILD_PATH:$LIB_PATH:$CLASSPATH"
 
 if [ ! \( $# = 1 -a "$1" = "run" \) ]; then
